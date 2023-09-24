9b26adff7574c76367498ac32c58a9e17617e535
Saswata Alex Majumder <majumder.ua@gmail.com>
2023-09-23 23:02:10 -0400

    control flow lab done

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let offer = '';
+  if (feet <= 400){
+    offer = 'This one is on me!';
+  } else if (feet <= 2000){
+    offer = 'That will be twenty bucks.';
+  } else if (feet <= 2500){
+    offer = 'I will gladly take your thirty bucks.';
+  } else {
+    offer = "No can do."
+  }
+  return offer;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let mesg = '';
+  city === 'NYC' ? (mesg = 'Ok, sounds good.') : (mesg = 'No go.');
+  return mesg;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let mesg = '';
+  switch (tip){
+    case 'generous': 
+      mesg = 'Thank you so much.';
+      break;
+    case 'not as generous': 
+      mesg = 'Thank you.';
+      break;
+    default: 
+      mesg = "Bye.";
+      break;
+  }
+  return mesg;
 }
